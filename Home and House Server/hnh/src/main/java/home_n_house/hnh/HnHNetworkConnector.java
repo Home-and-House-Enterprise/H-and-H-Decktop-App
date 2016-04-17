be913d98c94b3474da5da6af2ebe4bc4786ca693
Emanuel Peters <emanuelp7@gmail.com>
2016-04-17 00:17:01 -0400

    updated the server

--- Home and House Server/hnh/src/main/java/home_n_house/hnh/HnHNetworkConnector.java
+++ Home and House Server/hnh/src/main/java/home_n_house/hnh/HnHNetworkConnector.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Collection;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -31,41 +32,73 @@ public class HnHNetworkConnector implements Runnable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	   
+
 
 	}
 	public void run() {
 		String message="";
 		JSONParser parser = new JSONParser();
-	    while(!stop){
-	      try{
-	        message = input.readLine();
-	        JSONObject info=null;
-	        if(message!=null){
-	        	try {
-	        		info= (JSONObject)parser.parse(message);
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+		while(!stop){
+			try{
+				message = input.readLine();
+				JSONObject info=null;
+				if(message!=null){
+					try {
+						info= (JSONObject)parser.parse(message);
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					if(info!=null){//check if proper format
+						if(info.containsKey("messageType")){
+							String request=(String) info.get("messageType");
+							if(request.equals("request")){
+								processRequest(info);
+							}
+							if(request.equals("update")){
+								 processUpdate(info);
+							}
+							if(request.equals("alart")){
+								processAlart(info);
+							}
+
+						}
+
+					}
 				}
-	        	if(info!=null){//check if proper format
-	        		JSONObject outObj = new JSONObject();
-	        		outObj.put("status", "success");
-	        		output.println(outObj.toJSONString());
-	        	}
-	        }
-	      }
-	      catch (IOException e) {
-	        System.out.println("Client: "+IPAddress+" disconnected from server!");
-	        try{
-	          System.out.println("Closing connection");
-	          client.close();
-	        }catch(IOException e2){
-	          System.out.println("Error closing connection");
-	        }
-	        stop=true;
-	      }
-	    }
+			}
+			catch (IOException e) {
+				System.out.println("Client: "+IPAddress+" disconnected from server!");
+				try{
+					System.out.println("Closing connection");
+					client.close();
+				}catch(IOException e2){
+					System.out.println("Error closing connection");
+				}
+				stop=true;
+			}
+		}
 	}
 
+
+
+	@SuppressWarnings("unchecked")
+	private void processRequest(JSONObject request){
+		JSONObject outObj = new JSONObject();
+		outObj.put("status", "success");
+		output.println(outObj.toJSONString());
+	}
+
+	@SuppressWarnings("unchecked")
+	private void processUpdate(JSONObject request){
+		JSONObject outObj = new JSONObject();
+		outObj.put("status", "success");
+		output.println(outObj.toJSONString());
+	}
+	@SuppressWarnings("unchecked")
+	private void processAlart(JSONObject request){
+		JSONObject outObj = new JSONObject();
+		outObj.put("status", "success");
+		output.println(outObj.toJSONString());
+	}
 }
